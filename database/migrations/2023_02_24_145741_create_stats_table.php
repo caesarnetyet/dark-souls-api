43dc117c4da4eae491d42dd02a7949b35cde9c35
caesarnetyet <caesarnetyet@gmail.com>
2023-02-25 11:44:52 -0600

    upgrading laravel version

---
+++ database/migrations/2023_02_24_145741_create_stats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stats', function (Blueprint $table) {
+            $table->id();
+            $table->integer('vitality');
+            $table->integer('attunement');
+            $table->integer('endurance');
+            $table->integer('strength');
+            $table->integer('dexterity');
+            $table->integer('resistance');
+            $table->integer('intelligence');
+            $table->integer('faith');
+            $table->foreignId('character_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stats');
+    }
+};
