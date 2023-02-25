ea956990f66cf409062efd5de6e0cfb43c0b383a
caesarnetyet <caesarnetyet@gmail.com>
2023-02-25 11:50:28 -0600

    Renaming Models

--- app/Models/Classe.php
+++ app/Models/Classe.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class stat extends Model
+class Classe extends Model
 {
     use HasFactory;
 }
